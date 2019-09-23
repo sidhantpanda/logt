7c540b537bdbfc407edfc39d169130ab6789d3cc
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 00:33:23 +0530

    :white_check_mark: add test to check brand

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,52 +1,60 @@
 import LogT from '../src';
 
-describe('constructor sets values correctly', () => {
-  describe('sets log level correctly', () => {
-    test('sets correct numeric value of log level if string is supplied', () => {
-      const errorLogger = new LogT('error');
-      const warnLogger = new LogT('warn');
-      const infoLogger = new LogT('info');
-      const verboseLogger = new LogT('verbose');
-      const debugLogger = new LogT('debug');
-      const sillyLogger = new LogT('silly');
 
-      expect(errorLogger.getLogLevel()).toEqual(0);
-      expect(warnLogger.getLogLevel()).toEqual(1);
-      expect(infoLogger.getLogLevel()).toEqual(2);
-      expect(verboseLogger.getLogLevel()).toEqual(3);
-      expect(debugLogger.getLogLevel()).toEqual(4);
-      expect(sillyLogger.getLogLevel()).toEqual(5);
-    });
+test('sets correct numeric value of log level if string is supplied', () => {
+  const errorLogger = new LogT('error');
+  const warnLogger = new LogT('warn');
+  const infoLogger = new LogT('info');
+  const verboseLogger = new LogT('verbose');
+  const debugLogger = new LogT('debug');
+  const sillyLogger = new LogT('silly');
 
-    test('sets correct numeric value of log level if number is supplied', () => {
-      const errorLogger = new LogT(0);
-      const warnLogger = new LogT(1);
-      const infoLogger = new LogT(2);
-      const verboseLogger = new LogT(3);
-      const debugLogger = new LogT(4);
-      const sillyLogger = new LogT(5);
+  expect(errorLogger.getLogLevel()).toEqual(0);
+  expect(warnLogger.getLogLevel()).toEqual(1);
+  expect(infoLogger.getLogLevel()).toEqual(2);
+  expect(verboseLogger.getLogLevel()).toEqual(3);
+  expect(debugLogger.getLogLevel()).toEqual(4);
+  expect(sillyLogger.getLogLevel()).toEqual(5);
+});
 
-      expect(errorLogger.getLogLevel()).toEqual(0);
-      expect(warnLogger.getLogLevel()).toEqual(1);
-      expect(infoLogger.getLogLevel()).toEqual(2);
-      expect(verboseLogger.getLogLevel()).toEqual(3);
-      expect(debugLogger.getLogLevel()).toEqual(4);
-      expect(sillyLogger.getLogLevel()).toEqual(5);
-    });
+test('sets correct numeric value of log level if number is supplied', () => {
+  const errorLogger = new LogT(0);
+  const warnLogger = new LogT(1);
+  const infoLogger = new LogT(2);
+  const verboseLogger = new LogT(3);
+  const debugLogger = new LogT(4);
+  const sillyLogger = new LogT(5);
 
-    test('sets default log level of -1 if incorrect value supplied', () => {
-      // @ts-ignore
-      let logger = new LogT(-2);
-      expect(logger.getLogLevel()).toEqual(-1);
-      // @ts-ignore
-      logger = new LogT(10);
-      expect(logger.getLogLevel()).toEqual(-1);
-      // @ts-ignore
-      logger = new LogT('fefefe');
-      expect(logger.getLogLevel()).toEqual(-1);
-      // @ts-ignore
-      logger = new LogT();
-      expect(logger.getLogLevel()).toEqual(-1);
-    });
-  });
+  expect(errorLogger.getLogLevel()).toEqual(0);
+  expect(warnLogger.getLogLevel()).toEqual(1);
+  expect(infoLogger.getLogLevel()).toEqual(2);
+  expect(verboseLogger.getLogLevel()).toEqual(3);
+  expect(debugLogger.getLogLevel()).toEqual(4);
+  expect(sillyLogger.getLogLevel()).toEqual(5);
+});
+
+test('sets default log level of -1 if incorrect value supplied', () => {
+  // @ts-ignore
+  let logger = new LogT(-2);
+  expect(logger.getLogLevel()).toEqual(-1);
+  // @ts-ignore
+  logger = new LogT(10);
+  expect(logger.getLogLevel()).toEqual(-1);
+  // @ts-ignore
+  logger = new LogT('fefefe');
+  expect(logger.getLogLevel()).toEqual(-1);
+  // @ts-ignore
+  logger = new LogT();
+  expect(logger.getLogLevel()).toEqual(-1);
+});
+
+test('sets brand as null if not supplied', () => {
+  const logger = new LogT(0);
+  expect(logger.getBrand()).toBeNull();
+});
+
+test('sets brand correctly if supplied', () => {
+  const brand = 'panda';
+  const logger = new LogT(0, brand);
+  expect(logger.getBrand()).toBe(brand);
 });
