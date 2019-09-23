843b4996be89d9cc6b63f02dda093b7bc59f1549
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 00:46:56 +0530

    :sparkles: add basic console log handler

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,19 @@ const LOG_LEVELS = {
   silly: 5
 };
 
+interface ILogItem {
+  level: number;
+  message: string;
+  parts: any[]
+}
+
 export default class LogT {
   /** Log level, above which logs will be printed to console */
   private readonly logLevel: number = -1;
   /** Label for the log message, if any */
   private readonly brand: string | null = null;
+  /** Log history, which haven't yet been printed to console */
+  private history: ILogItem[] = [];
 
   constructor(logLevel: LOG_LEVEL, brand?: string) {
     // Check if logLevel value was supplied
@@ -36,31 +44,70 @@ export default class LogT {
     }
   }
 
+  private log(level: number, message: string, ...parts: any[]) {
+    if (level <= this.logLevel) {
+      switch (level) {
+        case LOG_LEVELS.error:
+          console.error(message, ...parts);
+          break;
+
+        case LOG_LEVELS.warn:
+          console.warn(message, ...parts);
+          break;
+
+        case LOG_LEVELS.info:
+          console.info(message, ...parts);
+          break;
+
+        case LOG_LEVELS.verbose:
+          console.log(message, ...parts);
+          break;
+
+        case LOG_LEVELS.debug:
+          console.log(message, ...parts);
+          break;
+
+        case LOG_LEVELS.silly:
+          console.log(message, ...parts);
+          break;
+
+        default:
+          console.log(message, ...parts);
+      }
+    } else {
+      this.history.push({
+        level,
+        message,
+        parts
+      });
+    }
+  }
+
   public getLogLevel = (): number => this.logLevel;
 
   public getBrand = (): string | null => this.brand;
 
   public error = (message: string, ...parts: any[]) => {
-    console.error(message, ...parts);
+    this.log(LOG_LEVELS.error, message, ...parts);
   };
 
   public warn = (message: string, ...parts: any[]) => {
-    console.warn(message, ...parts);
+    this.log(LOG_LEVELS.warn, message, ...parts);
   };
 
   public info = (message: string, ...parts: any[]) => {
-    console.info(message, ...parts);
+    this.log(LOG_LEVELS.info, message, ...parts);
   };
 
   public verbose = (message: string, ...parts: any[]) => {
-    console.log(message, ...parts);
+    this.log(LOG_LEVELS.verbose, message, ...parts);
   };
 
   public debug = (message: string, ...parts: any[]) => {
-    console.debug(message, ...parts);
+    this.log(LOG_LEVELS.debug, message, ...parts);
   };
 
   public silly = (message: string, ...parts: any[]) => {
-    console.log(message, ...parts);
+    this.log(LOG_LEVELS.silly, message, ...parts);
   };
 }
