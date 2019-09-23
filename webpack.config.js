07db0499cb73eace6c150d0ceb756a0426b07fd6
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 01:06:04 +0530

    :wrench: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.webpack.json"
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "logt.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
