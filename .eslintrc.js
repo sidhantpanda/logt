dbba2480f873747f68e52d947c5e7b81e2194370
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 18:01:37 +0530

    :wrench: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "arrow-parens": [2, "as-needed"]
+  }
+};
