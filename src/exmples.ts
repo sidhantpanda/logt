d1a055448b78ae604610e8414e832bcf3f42d235
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 17:44:53 +0530

    :pencil: add examples

---
+++ src/exmples.ts
@@ -0,0 +1,15 @@
+import LogT from './lib/logt';
+
+const TAG = 'main.xs';
+
+const runExamples = () => {
+  const logger = new LogT(5);
+  logger.error(TAG, new Error('example error'));
+  logger.warn(TAG, 'example warning');
+  logger.info(TAG, 'example info');
+  logger.verbose(TAG, 'example verbose');
+  logger.debug(TAG, 'example debug');
+  logger.silly(TAG, 'example silly');
+}
+
+export default runExamples;
