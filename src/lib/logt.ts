67c722fa04c106404e250b53622bc115d76ab857
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 17:02:37 +0530

    :sparkles: add styled labels to logs

--- src/lib/logt.ts
+++ src/lib/logt.ts
@@ -10,6 +10,16 @@ const LOG_LEVELS = {
   silly: 5
 };
 
+const STYLES = {
+  error: 'color: white; background: #db2828; border-radius: 8px',
+  warn: 'color: white; background: #fbbd08; border-radius: 8px',
+  info: 'color: white; background: #c4c64f; border-radius: 8px',
+  verbose: 'color: white; background: #6435c9; border-radius: 8px',
+  debug: 'color: white; background: #2185d0; border-radius: 8px',
+  silly: 'color: white; background: #21ba45; border-radius: 8px',
+  tag: 'color: black; background: #5bfff4; border-radius: 8px',
+};
+
 interface ILogItem {
   level: number;
   tag: string;
@@ -23,7 +33,7 @@ export default class LogT {
   /** Log history, which haven't yet been printed to console */
   private history: ILogItem[] = [];
 
-  constructor(logLevel: LOG_LEVEL, brand?: string) {
+  constructor(logLevel: LOG_LEVEL) {
     this.setLogLevel(logLevel);
   }
 
@@ -31,27 +41,27 @@ export default class LogT {
     if (level <= this.logLevel) {
       switch (level) {
         case LOG_LEVELS.error:
-          console.error(tag, message, ...parts);
+          console.error(`%c error %c %c ${tag} `, STYLES.error, '', STYLES.tag, message, ...parts);
           break;
 
         case LOG_LEVELS.warn:
-          console.warn(tag, message, ...parts);
+          console.warn(`%c warn %c %c ${tag} `, STYLES.warn, '', STYLES.tag, message, ...parts);
           break;
 
         case LOG_LEVELS.info:
-          console.info(tag, message, ...parts);
+          console.info(`%c info %c %c ${tag} `, STYLES.info, '', STYLES.tag, message, ...parts);
           break;
 
         case LOG_LEVELS.verbose:
-          console.log(tag, message, ...parts);
+          console.log(`%c verbose %c %c ${tag} `, STYLES.verbose, '', STYLES.tag, message, ...parts);
           break;
 
         case LOG_LEVELS.debug:
-          console.log(tag, message, ...parts);
+          console.log(`%c debug %c %c ${tag} `, STYLES.debug, '', STYLES.tag, message, ...parts);
           break;
 
         case LOG_LEVELS.silly:
-          console.log(tag, message, ...parts);
+          console.log(`%c silly %c %c ${tag} `, STYLES.silly, '', STYLES.tag, message, ...parts);
           break;
 
         default:
@@ -116,11 +126,7 @@ export default class LogT {
     this.history = [];
 
     currentHistory.forEach(logItem => {
-      if (logItem.parts.length > 0) {
-        this.log(logItem.level, logItem.tag, logItem.message, logItem.parts);
-      } else {
-        this.log(logItem.level, logItem.tag, logItem.message);
-      }
+      this.log(logItem.level, logItem.tag, logItem.message, ...logItem.parts);
     });
 
     this.logLevel = oldLogLevel;
