3bb5300b86932fe11be8566ce57d7c5946a62e56
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-24 14:43:53 +0530

    :wrench: add webpack config for development

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const merge = require("webpack-merge");
+const prodConfig = require("./webpack.config");
+
+const devConfig = merge(prodConfig, {
+  mode: 'development',
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    port: 3000
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement"
+    })
+  ]
+});
+
+module.exports = devConfig;
