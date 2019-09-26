f8001073e2dc09393b1d9681c9d18167f1b3a9e9
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-27 00:07:57 +0530

    :package: v1.2.0

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/** Log level string to number map */
 const LOG_LEVELS = {
     none: -1,
     error: 0,
@@ -9,6 +10,7 @@ const LOG_LEVELS = {
     debug: 4,
     silly: 5,
 };
+/** All CSS requried to print labels */
 const STYLES = {
     error: 'color: white; background: #db2828; border-radius: 8px',
     warn: 'color: white; background: #fbbd08; border-radius: 8px',
@@ -18,42 +20,61 @@ const STYLES = {
     silly: 'color: white; background: #21ba45; border-radius: 8px',
     tag: 'color: black; background: #5bfff4; border-radius: 8px',
 };
+/** Logger Class */
 class LogT {
+    /**
+     * Create a LogT instance
+     * @param logLevel Logger will print logs to consoles with level less than equal to this
+     */
     constructor(logLevel) {
         /** Log level, above which logs will be printed to console */
         this.logLevel = LOG_LEVELS.none;
         /** Logs which are hidden - not been printed to console */
         this.hidden = [];
+        /** Original `console.error` method */
+        // eslint-disable-next-line no-console
+        this.originalError = console.error;
+        /** Original `console.warn` method */
+        // eslint-disable-next-line no-console
+        this.originalWarn = console.warn;
+        /** Original `console.info` method */
+        // eslint-disable-next-line no-console
+        this.originalInfo = console.info;
+        /** Original `console.log` method */
+        // eslint-disable-next-line no-console
+        this.originalLog = console.log;
+        /**
+         * Internal log method which prints valid logs
+         * to console and add a higher level log to {LogT.hidden}
+         * @param level Level of the log message
+         * @param tag Tag for log
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.log = (level, tag, message, ...parts) => {
             if (level <= this.logLevel) {
                 switch (level) {
                     case LOG_LEVELS.error:
-                        // eslint-disable-next-line no-console
-                        console.error(`%c error %c %c ${tag} `, STYLES.error, '', STYLES.tag, message, ...parts);
+                        this.originalError(`%c error %c %c ${tag} `, STYLES.error, '', STYLES.tag, message, ...parts);
                         break;
                     case LOG_LEVELS.warn:
-                        // eslint-disable-next-line no-console
-                        console.warn(`%c warn %c %c ${tag} `, STYLES.warn, '', STYLES.tag, message, ...parts);
+                        this.originalWarn(`%c warn %c %c ${tag} `, STYLES.warn, '', STYLES.tag, message, ...parts);
                         break;
                     case LOG_LEVELS.info:
-                        // eslint-disable-next-line no-console
-                        console.info(`%c info %c %c ${tag} `, STYLES.info, '', STYLES.tag, message, ...parts);
+                        this.originalInfo(`%c info %c %c ${tag} `, STYLES.info, '', STYLES.tag, message, ...parts);
                         break;
                     case LOG_LEVELS.verbose:
-                        // eslint-disable-next-line no-console
-                        console.log(`%c verbose %c %c ${tag} `, STYLES.verbose, '', STYLES.tag, message, ...parts);
+                        this.originalLog(`%c verbose %c %c ${tag} `, STYLES.verbose, '', STYLES.tag, message, ...parts);
                         break;
                     case LOG_LEVELS.debug:
-                        // eslint-disable-next-line no-console
-                        console.log(`%c debug %c %c ${tag} `, STYLES.debug, '', STYLES.tag, message, ...parts);
+                        this.originalLog(`%c debug %c %c ${tag} `, STYLES.debug, '', STYLES.tag, message, ...parts);
                         break;
                     case LOG_LEVELS.silly:
-                        // eslint-disable-next-line no-console
-                        console.log(`%c silly %c %c ${tag} `, STYLES.silly, '', STYLES.tag, message, ...parts);
+                        this.originalLog(`%c silly %c %c ${tag} `, STYLES.silly, '', STYLES.tag, message, ...parts);
                         break;
                     default:
                         // eslint-disable-next-line no-console
-                        console.log(tag, message, ...parts);
+                        this.originalLog(tag, message, ...parts);
                 }
             }
             else {
@@ -65,7 +86,15 @@ class LogT {
                 });
             }
         };
+        /**
+         * Get instance log level
+         */
         this.getLogLevel = () => this.logLevel;
+        /**
+         * Set instance log level.
+         * @param logLevel Log level set on instance.
+         * Logs which have levels less than or equal to this value will be printed to console
+         */
         this.setLogLevel = (logLevel) => {
             // Check if logLevel value was supplied
             if (logLevel != null) {
@@ -81,24 +110,64 @@ class LogT {
                 }
             }
         };
+        /**
+         * Helper to print error logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.error = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.error, tag, message, ...parts);
         };
+        /**
+         * Helper to print warning logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.warn = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.warn, tag, message, ...parts);
         };
+        /**
+         * Helper to print info logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.info = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.info, tag, message, ...parts);
         };
+        /**
+         * Helper to print verbose logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.verbose = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.verbose, tag, message, ...parts);
         };
+        /**
+         * Helper to print debug logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.debug = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.debug, tag, message, ...parts);
         };
+        /**
+         * Helper to print silly logs
+         * @param tag Log tag
+         * @param message Log message
+         * @param parts Any other arguments to be passed on to `console`
+         */
         this.silly = (tag, message, ...parts) => {
             this.log(LOG_LEVELS.silly, tag, message, ...parts);
         };
+        /**
+         * Method to print logs hidden due higher log level than set on the instance
+         * @param logLevel Logs with less than or equal to this will be printed to console
+         */
         this.showHidden = (logLevel) => {
             const oldLogLevel = this.logLevel;
             this.setLogLevel(logLevel);
@@ -108,6 +177,28 @@ class LogT {
                 this.log(logItem.level, logItem.tag, logItem.message, ...logItem.parts);
             });
             this.setLogLevel(oldLogLevel);
+        };
+        /**
+         * Method to override `window.console` methods
+         */
+        this.readConsole = () => {
+            const TAG = 'console';
+            // eslint-disable-next-line no-console
+            console.error = (message, ...parts) => {
+                this.log(LOG_LEVELS.error, TAG, message, ...parts);
+            };
+            // eslint-disable-next-line no-console
+            console.warn = (message, ...parts) => {
+                this.log(LOG_LEVELS.warn, TAG, message, ...parts);
+            };
+            // eslint-disable-next-line no-console
+            console.info = (message, ...parts) => {
+                this.log(LOG_LEVELS.info, TAG, message, ...parts);
+            };
+            // eslint-disable-next-line no-console
+            console.log = (message, ...parts) => {
+                this.log(LOG_LEVELS.debug, TAG, message, ...parts);
+            };
         };
         this.setLogLevel(logLevel);
     }
